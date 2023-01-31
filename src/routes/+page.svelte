2a06673abdff44fc212ad4873bccc2abc4869c0f
siravijbb <siravijbb@gmail.com>
2023-01-31 22:32:53 +0700

    dsdsd

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Navbar from './components/navbar.svelte';
+</script>
+  
+
+<div class="warper mx-4">
+  <Navbar />
+    
+    <main class="Maincon box-border bg-gray-300 m4 rounded-xl p-4 shadow-xl">
+        <h1 class="Header text-9xl text-center pt-10">NB GEARS</h1>
+        <img src="/pic.webp" class="s3 max-w-screen-lg mx-auto pb-10 rounded-3xl" alt="me"   />
+        <p>     I am an failure analysis engineer, Who looking for new challanges from past experience from sattlelite PCB to a new challenges. In web programing, I'm currently learning web technologies with using some experience with coding in school.
+                Also when I had free time, I am a maker who made phototype of things that I wanted to combine to reduce size, Over the past 3 years,<br>I have been hosting server as additional hobby which made me familar with networking. <br>And also I owned scooter for using in Bangkok for reducing and precise timing in life schedual.</p>
+    </main>
+
+    </div>
+    <footer class="mt-12 mb-8 max-w-7xl mx-auto px-4 md:px-6 flex items-center text-gray-600 text-sm sm:text-base">
+        <div class="space-x-2 flex">
+          <a
+            href="https://creatorsgarten.org/ring"
+            title="Creatorsgarten"
+          >
+            <img src="/garten.svg" class="w-6 sm:w-8 aspect-square" alt="webring" />
+          </a>
+        </div>
+        <div class="mx-4 h-6 sm:h-8 border-l-2 border-gray-300" />
+        <p>© {new Date().getFullYear()} Phumrapee Limpianchop</p>
+      </footer>
+
+    <style>
+    .warper{
+        font-family: 'IBM Plex Sans Thai', sans-serif;
+        
+    
+    }
+    .Maincon{
+            
+        font-size: 1.875rem;
+            max-width: 1280px ;
+            margin: 40px auto;
+    }
+    </style>
+    
---
+++ src/routes/components/navbar.svelte
@@ -0,0 +1,11 @@
+<nav class="sticky top-0 w-full max-w-7xl mx-auto my-6 px-4 md:px-6 navbar dark:bg-gray-900 rounded-2xl shadow-lg text-white">
+	<div class="flex justify-between max-w-screen-xl mx-auto">
+		<div class="flex items-center space-x-28">
+				<h1 class="Header text-5xl item-center 	py-5">NB GEARS</h1>
+		</div>
+		<div class="flex items-center space-x-14">
+			<a href="/">Login</a>
+			<button class="primary">Start Free Trial</button>
+		</div>
+	</div>
+</nav>
