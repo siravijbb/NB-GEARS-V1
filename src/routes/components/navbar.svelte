26ed007775ebc0e0a3f7f85e68837b792c752382
siravijbb <siravijbb@gmail.com>
2023-02-12 02:07:18 +0700

    improvement:added google twitter facebbok seach engine

--- src/routes/components/navbar.svelte
+++ src/routes/components/navbar.svelte
@@ -1,6 +1,42 @@
 <nav
 	class=" top-0 w-full  mx-auto navbar bg-gray-900 rounded-2xl shadow-lg text-white max-w-7xl min-w-sm"
 >
+	<meta name="googlebot" content="index, follow" />
+	<meta
+		name="keywords"
+		content="NB GEARS, nb gears, North Bangkok GEEKS ENGINEERING, NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS, First FRC, First"
+	/>
+	<meta
+		name="image"
+		content="https://res.cloudinary.com/dwvwjxizk/image/upload/v1641491432/banner_b4lbdc.png"
+	/>
+	<meta itemProp="name" content="NB GEARS" />
+	<meta
+		itemProp="description"
+		content="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS"
+	/>
+	<meta property="og:title" content="NB GEARS" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="www.nbgears.tech" />
+	<meta property="og:image" content="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS" />
+	<meta
+		property="og:description"
+		content="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS"
+	/>
+	<meta itemProp="name" content="NB GEARS" />
+	<meta
+		itemProp="description"
+		content="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS"
+	/>
+	<meta itemProp="image" content="NR_Gear_logo_white.png" />
+	<meta name="twitter:card" value="summary_large_image" />
+	<meta name="twitter:title" value="NB GEARS" />
+	<meta
+		name="twitter:description"
+		value="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS"
+	/>
+	<meta name="twitter:image" value="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS" />
+
 	<div class="max-w-7xl mx-auto my-6 px-4 md:px-6">
 		<nav class=" bg-base-100 rounded-2xl shadow-lg">
 			<div class="flex justify-between max-w-screen-xl mx-auto">
