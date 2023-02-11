bb562ba005647503cec82884c13cb1c3d3bf6284
siravijbb <siravijbb@gmail.com>
2023-02-11 13:54:19 +0700

    contented added:added text to about us

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -13,7 +13,7 @@
             <div class="" style="Bigcon object-scale-down mx-auto pb-10 rounded-3xl ">
                 <h1 class="text-5xl margin: 40px; Bigcon">About Us</h1>
                 <img src="NR_Gear_logo_white.png" class="w-full md:block md:w-auto flex items-center space-x-28 p-5 bg-gray-900 rounded-2xl" alt="NORTH BANGKOK GEEKS ENGINEERING AWESOME ROBOTIC SOLUTIONS" />
-                <p class="Maincon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Our story began by Mr.Punroon who was veteran member of NC&nbsp;GEARS, He was paticipated in FTC 2021 which he has a dream of FTC Team in thailand in our school.</p>
+                <p class="Maincon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Our story began by Mr.Punroon who was veteran member of NC&nbsp;GEARS, He was paticipated in FTC 2021 which he has a dream of FTC Team in thailand in our school.he wants to create a team to inspire the newer generation of young people to have experience with robotic events and improve their engineering skill in practical applications. One of our goals is to prove that teenagers that live in Bangkok also have the potential for competition. Via a connection with the NC GEARS team, they are able to give us the guidance into starting a proper team. Our team's name, "NB GEARS: stands for "North Bangkok Geeks Engineering Awesome Robotic Solutions" similar to our advising team.</p>
 
               
                     
